35d3174b3da34a7e3cb89bc09e9bf58e30848486
eldarial <darialdo92@hotmail.com>
2018-01-19 00:53:04 -0500

    added code and README

---
+++ DefuseBomb/DefuseBomb/DefuseBomb/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DefuseBomb
+{
+	public partial class MainPage : ContentPage
+	{
+        //static 
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        async void Buttonclicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            Entry defuseEntry = this.FindByName<Entry>("defuseEntry");
+            Label dLabel = this.FindByName<Label>("defuseLabel");
+            System.Diagnostics.Debug.WriteLine("here: " + defuseEntry.Text);
+            dLabel.Text = defuseEntry.Text;
+        }
+
+	}
+}
